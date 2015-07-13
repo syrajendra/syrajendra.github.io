ea0951128786ee8fc91ce605bab155c9c72ea5b1
syrajendra <syrajendra@juniper.net>
2015-07-13 19:02:20 +0530

    Updated

--- rSync/js/dropbox.js
+++ rSync/js/dropbox.js
@@ -58,7 +58,7 @@ function load_dropbox(cloud_name, parent) {
 	var client = new Dropbox.Client({ key: "0uas9683o65och3" });
 	console.log(client.isAuthenticated());
 	if(!client.isAuthenticated()) {
-		client.authDriver(new Dropbox.AuthDriver.Redirect({rememberUser:true, redirectUrl:'https://115.99.249.252/rSync/index.html'}));
+		client.authDriver(new Dropbox.AuthDriver.Redirect({rememberUser:true, redirectUrl:'https://syrajendra.github.io/rSync/index.html'}));
 		client.authenticate(function (error, data) {
 			if(error) {
 				console.error('Error: Failed to get access ' + error);
